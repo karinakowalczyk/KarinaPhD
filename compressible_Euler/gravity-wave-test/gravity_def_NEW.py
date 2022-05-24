143d96040e3a3b53c58bfb4dc11987109373099f
karinakowalczyk <49185050+karinakowalczyk@users.noreply.github.com>
2022-05-24 15:19:14 +0100

    Add files via upload

--- compressible_Euler/gravity-wave-test/gravity_def_NEW.py
+++ compressible_Euler/gravity-wave-test/gravity_def_NEW.py
@@ -8,7 +8,7 @@ import sys
 '''
 
 
-#set up mesh and parameters for main computations
+# set up mesh and parameters for main computations
 dT = Constant(0)  # to be set later
 parameters = Parameters()
 g = parameters.g
@@ -20,7 +20,7 @@ L = 3.0e5
 H = 1.0e4  # Height position of the model top
 delx = 1000
 delz = 1000
-nlayers = H/delz # horizontal layers
+nlayers = H/delz  # horizontal layers
 columns = L/delx  # number of columns
 m = PeriodicIntervalMesh(columns, L)
 mesh = ExtrudedMesh(m, layers=nlayers, layer_height=H/nlayers)
@@ -35,11 +35,11 @@ H = Constant(H)
 V0, _, Vp, Vt, Vtr = build_spaces(mesh, vertical_degree=1, horizontal_degree=1)
 W = V0*Vp*Vt*Vtr
 
-#initialise background temperature
+# initialise background temperature
 # N^2 = (g/theta)dtheta/dz => dtheta/dz = theta N^2g => theta=theta_0exp(N^2gz)
 Tsurf = 300.
 N = parameters.N
-x,z = SpatialCoordinate(mesh)
+x, z = SpatialCoordinate(mesh)
 thetab = Tsurf*exp(N**2*z/g)
 theta_b = Function(Vt).interpolate(thetab)
 
@@ -52,10 +52,10 @@ lambdar_b = Function(Vtr)
 compressible_hydrostatic_balance_with_correct_pi_top(mesh, parameters, theta_b, rho_b, lambdar_b, Pi)
 
 
-#initialise functions for full Euler solver
+# initialise functions for full Euler solver
 xc = L/2
 a = Constant(5000.)
-thetab_pert= 0.01*sin(pi*z/H)/(1+(x-xc)**2/a**2)
+thetab_pert = 0.01*sin(pi*z/H)/(1+(x-xc)**2/a**2)
 theta0 = Function(Vt, name="theta0").interpolate(theta_b + thetab_pert)
 
 rho0 = Function(Vp, name="rho0").interpolate(rho_b)  # where rho_b solves the hydrostatic balance eq.
@@ -67,7 +67,7 @@ lambdar0 = Function(Vtr, name="lambdar0").assign(lambdar_b)  # we use lambda fro
 zvec = as_vector([0,1])
 n = FacetNormal(mesh)
 
-Un =Function(W)
+Un = Function(W)
 Unp1 = Function(W)
 un, rhon, thetan, lambdarn = Un.split()
 unp1, rhonp1, thetanp1, lambdarnp1 = split(Unp1)
@@ -81,61 +81,62 @@ Pin = thermodynamics_pi(parameters, rhon, thetan)
 Pinp1 = thermodynamics_pi(parameters, rhonp1, thetanp1)
 Pinph = 0.5*(Pin + Pinp1)
 
-#functions for the upwinding terms
+# functions for the upwinding terms
 unn = 0.5*(dot(unph, n) + abs(dot(unph, n)))  # used for the upwind-term
 Upwind = 0.5*(sign(dot(unph, n))+1)
 perp_u_upwind = lambda q: Upwind('+')*perp(q('+')) + Upwind('-')*perp(q('-'))
 u_upwind = lambda q: Upwind('+')*q('+') + Upwind('-')*q('-')
 
 
-
 def uadv_eq(w):
-    return( -inner(perp(grad(inner(w, perp(unph)))), unph)*dx
-                     - inner(jump(  inner(w, perp(unph)), n), perp_u_upwind(unph))*(dS)
-                     #- inner(inner(w, perp(unph))* n, unph) * ( ds_t + ds_b )
-                     - 0.5 * inner(unph, unph) * div(w) * dx
-                     #+ 0.5 * inner(u_upwind(unph), u_upwind(unph)) * jump(w, n) * dS_h
-             )
+    return(-inner(perp(grad(inner(w, perp(unph)))), unph)*dx
+           - inner(jump(inner(w, perp(unph)), n), perp_u_upwind(unph))*(dS)
+           # - inner(inner(w, perp(unph))* n, unph) * ( ds_t + ds_b )
+           - 0.5 * inner(unph, unph) * div(w) * dx
+           # + 0.5 * inner(u_upwind(unph), u_upwind(unph)) * jump(w, n) * dS_h
+           )
+
 
 def u_eqn(w, gammar):
-    return ( inner(w, unp1 - un)*dx + dT* (
-                uadv_eq(w) 
-                - c_p*div(w*thetanph)* Pinph*dx
-                + c_p*jump(thetanph*w, n)*lambdarnp1('+')*dS_h
-                + c_p*inner(thetanph*w, n)*lambdarnp1*(ds_t + ds_b)
-                + c_p*jump(thetanph*w, n)*(0.5*(Pinph('+') + Pinph('-')))*(dS_v)
-                #+ c_p * inner(thetanph * w, n) * Pinph * (ds_v)
-                + gammar('+')*jump(unph,n)*dS_h
-                + gammar*inner(unph,n)*(ds_t + ds_b)
-                + g * inner(w,zvec)*dx)
-                )
+    return (inner(w, unp1 - un)*dx + dT * (
+            uadv_eq(w)
+            - c_p*div(w*thetanph)*Pinph*dx
+            + c_p*jump(thetanph*w, n)*lambdarnp1('+')*dS_h
+            + c_p*inner(thetanph*w, n)*lambdarnp1*(ds_t + ds_b)
+            + c_p*jump(thetanph*w, n)*(0.5*(Pinph('+') + Pinph('-')))*dS_v
+            # + c_p * inner(thetanph * w, n) * Pinph * (ds_v)
+            + gammar('+')*jump(unph, n)*dS_h
+            + gammar*inner(unph, n)*(ds_t + ds_b)
+            + g * inner(w, zvec)*dx)
+            )
 
 
 dS = dS_h + dS_v
+
+
 def rho_eqn(phi):
-    return ( phi*(rhonp1 - rhon)*dx 
-                + dT * (-inner(grad(phi), rhonph*unph)*dx
-                + (phi('+') - phi('-'))*(unn('+')*rhonph('+') - unn('-')*rhonph('-'))*dS
-                #+ dot(phi*unph,n) *ds_v
+    return (phi*(rhonp1 - rhon)*dx
+            + dT * (-inner(grad(phi), rhonph*unph)*dx
+                    + (phi('+') - phi('-'))*(unn('+')*rhonph('+') - unn('-')*rhonph('-'))*dS
+                    # + dot(phi*unph,n) *ds_v
                     )
             )
 
 
 def theta_eqn(chi):
-    return (chi*(thetanp1 - thetan)*dx  
-                    + dT * (inner(chi*unph, grad(thetanph))*dx
+    return (chi*(thetanp1 - thetan)*dx
+            + dT * (inner(chi*unph, grad(thetanph))*dx
                     + (chi('+') - chi('-'))* (unn('+')*thetanph('+') - unn('-')*thetanph('-'))*dS
                     - dot(chi('+')*thetanph('+')*unph('+'),  n('+'))*dS - inner(chi('-')*thetanph('-')*unph('-'), n('-'))*dS
-
-                    #+ dot(unph*chi,n)*thetanph * (ds_v + ds_t + ds_b)
-                    #- inner(chi*thetanph * unph, n)* (ds_v +  ds_t + ds_b)
+                    # + dot(unph*chi,n)*thetanph * (ds_v + ds_t + ds_b)
+                    # - inner(chi*thetanph * unph, n)* (ds_v +  ds_t + ds_b)
                     )
             )
 
 
 # set up test functions and the nonlinear problem
 w, phi, chi, gammar = TestFunctions(W)
-#a = Constant(10000.0)
+# a = Constant(10000.0)
 eqn = u_eqn(w, gammar) + theta_eqn(chi) + rho_eqn(phi) # + gamma * rho_eqn(div(w))
 nprob = NonlinearVariationalProblem(eqn, Unp1)
 
@@ -185,8 +186,8 @@ file_out = File(name+'.pvd')
 
 rhon_pert = Function(Vp)
 thetan_pert = Function(Vt)
-rhon_pert.assign(rhon - rho0)
-thetan_pert.assign(thetan - theta0)
+rhon_pert.assign(rhon - rho_b)
+thetan_pert.assign(thetan - theta_b)
 
 file_out.write(un, rhon_pert, thetan_pert)
 
@@ -204,22 +205,20 @@ while t < tmax - 0.5*dt:
     print(t)
     t += dt
     tdump += dt
-    
+
     nsolver.solve()
 
     Un.assign(Unp1)
 
-    rhon_pert.assign(rhon - rho0)
-    thetan_pert.assign(thetan - theta0)
+    rhon_pert.assign(rhon - rho_b)
+    thetan_pert.assign(thetan - theta_b)
 
-    print("rho max min pert", rhon_pert.dat.data.max(),  rhon_pert.dat.data.min())
+    print("rho max min pert", rhon_pert.dat.data.max(), rhon_pert.dat.data.min())
     print("theta max min pert", thetan_pert.dat.data.max(), thetan_pert.dat.data.min())
-    
+
     if tdump > dumpt - dt*0.5:
         file_out.write(un, rhon_pert, thetan_pert)
 
-        #file_gw.write(un, rhon, thetan, lambdarn)
-        #file2.write(un_pert, rhon_pert, thetan_pert)
+        # file_gw.write(un, rhon, thetan, lambdarn)
+        # file2.write(un_pert, rhon_pert, thetan_pert)
         tdump -= dumpt
-
-
