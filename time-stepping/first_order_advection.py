045e98ff0c56322a4c899ef68281f6efc521c266
Kowalczyk <karina.kwlczyk@gmail.com>
2023-04-28 16:32:29 +0100

    few adjustments

--- time-stepping/first_order_advection.py
+++ time-stepping/first_order_advection.py
@@ -1,5 +1,7 @@
 from firedrake import *
-#mesh parameters
+import math
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 def RT_Element_broken_ext(mesh, horizontal_degree, vertical_degree):
     '''
@@ -22,18 +24,24 @@ def RT_Element_broken_ext(mesh, horizontal_degree, vertical_degree):
     
     return V2_elt
    
+def plot_fd_vector(function, name):
+    fig, axes = plt.subplots()
+    quiver(function, axes=axes)
+    axes.set_aspect("equal")
+    axes.set_title(name)
+    fig.savefig(name)
 
-#L = 100000.
-#H = 30000.  # Height position of the model top
-#delx = 500*2
-#delz = 250*2
-#nlayers = H/delz  # horizontal layers
-#columns = L/delx  # number of columns
+def plot_fd_scalar(function, file_name):
+    # We first set up a figure and axes and draw the first frame. ::
+    nsp=16
+    fig, axes = plt.subplots()
+    axes.set_aspect('equal')
+    colors = tripcolor(function, num_sample_points=nsp, vmin=1, vmax=2, axes=axes)
+    fig.colorbar(colors)
+    fig.savefig(file_name)
 
-# build mesh
-#m = PeriodicIntervalMesh(columns, L)
-#mesh = ExtrudedMesh(m, layers=nlayers, layer_height=H/nlayers)
-mesh = UnitSquareMesh(40,40)
+
+mesh = PeriodicUnitSquareMesh(40,40)
 
 n = FacetNormal(mesh)
 
@@ -46,53 +54,161 @@ V2 = FunctionSpace(mesh, "DG", 1)
 W = MixedFunctionSpace((V1_broken,V2))
 
 #set up test and trial functions
-U_star = Function(W)
-u_star, D_star = split(U_star)
-un=Function(V1).interpolate(as_vector([0.0,0.0]))
-Dn=Function(V2).interpolate(Constant(0.))
+u = Function(V1_broken)
+D= Function(V2)
 
-w, phi = TestFunctions(W)
+#initialise u, D, will hold un, Dn later
+x, y = SpatialCoordinate(mesh)
+#velocity = as_vector((0.5 - y, x - 0.5))
+velocity = as_vector((0.3,0))
+ubar = Function(V1).interpolate(velocity)
+
+plot_fd_vector(ubar, 'ubar')
+
+#cosine-bell as initial condition for D
+bell_r0 = 0.15; bell_x0 = 0.25; bell_y0 = 0.5
+bell = 0.4*(1+cos(math.pi*min_value(sqrt(pow(x-bell_x0, 2) + pow(y-bell_y0, 2))/bell_r0, 1.0)))
+D.interpolate(1+bell)
+
+plot_fd_scalar(D, "D_init")
+
+D_init = Function(V2).assign(D)
+Dbar = Function(V2).assign(0.)
+
+
+#D.interpolate(Constant(0.))
+Ds = []
+us = []
+
+
+T = 1.
+dt = T/100.0
+dtc = Constant(dt)
+#q_in = Constant(1.0)
+
+w = TestFunction(V1_broken)
+phi = TestFunction(V2)
 
 #set up equations
-uup = lambda u: 0.5 * (dot(u, n) + abs(dot(u, n)))
-
 def perp_u_upwind(u):
     Upwind = 0.5 * (sign(dot(u, n)) + 1)
     return Upwind('+')*perp(u('+')) + Upwind('-')*perp(u('-'))
 
-def adv_eq(u, ubar, w):
+def adv_u(ubar):
+    un = 0.5*(dot(ubar, n) + abs(dot(ubar, n)))
+
+    return(-inner(div(outer(w, ubar)), u)*dx
+           +dot(jump(w), (un('+')*u('+') - un('-')*u('-')))*dS
+    )
+
+def adv_eq_vector_id(u, ubar, w):
     return (
         + -inner(perp(grad(inner(w, perp(ubar)))), u)*dx
         - inner(jump(inner(w, perp(ubar)), n), perp_u_upwind(u))*(dS)
         - inner(ubar, u) * div(w) * dx
         )
 
-def eq_D(D, Dbar, ubar, phi):
+def eq_D(Dbar, ubar):
     h = D-Dbar
     uup = 0.5 * (dot(ubar, n) + abs(dot(ubar, n)))
-    return (- inner(grad(phi), ubar)*h*dx
-            + jump(phi)*(uup('+')*h('+')
-                            - uup('-')*h('-'))*(dS))
+    return (- inner(grad(phi), outer(D,ubar))*dx
+            + dot(jump(phi),(uup('+')*D('+')
+                            - uup('-')*D('-')))*(dS)
+    )
 
-ubar = Function(V1).assign(un)
-Dbar = Dn
-delT = 1.
+#equations are independent
+L1_u = dtc*adv_u(ubar)
+L1_D = dtc*eq_D(Dbar,ubar)
 
-eqn = inner(w, u_star - un)*dx  + delT*adv_eq(u_star, ubar, w) + inner(phi, D_star - Dn)*dx + delT*eq_D(D_star, Dbar, ubar, phi)
-prob = NonlinearVariationalProblem(eqn, U_star)
-sparameters_exact = { "mat_type": "aij",
-                    'snes_monitor': None,
-                    'snes_stol': 1e-50,
-                    #'snes_view': None,
-                    #'snes_type' : 'ksponly',
-                    'ksp_monitor_true_residual': None,
-                    'snes_converged_reason': None,
-                    'ksp_converged_reason': None,
-                    "ksp_type" : "preonly",
-                    "pc_type" : "ilu",
-                    #"pc_factor_mat_solver_type": "mumps"
-                    }
-solver = NonlinearVariationalSolver(prob, solver_parameters=sparameters_exact)
-#just one time-step
-solver.solve()
+#for the second and third step of SSPRK
+u1 = Function(V1_broken); u2 = Function(V1_broken)
+L2_u = replace(L1_u, {u: u1}); L3_u = replace(L1_u, {u: u2})
 
+D1 = Function(V2); D2 = Function(V2)
+L2_D = replace(L1_D, {D: D1}); L3_D = replace(L1_D, {D: D2})
+
+#time derivative term involves just a mass matrix
+du = TrialFunction(V1_broken)
+a_u = inner(du,w)*dx
+
+dD = TrialFunction(V2)
+a_D = dD*phi*dx
+
+#function to hold current increment of u
+du = Function(V1_broken)
+params = {'ksp_type': 'preonly', 'pc_type': 'bjacobi', 'sub_pc_type': 'ilu'}
+prob_u1 = LinearVariationalProblem(a_u, L1_u, du)
+solv_u1 = LinearVariationalSolver(prob_u1, solver_parameters=params)
+prob_u2 = LinearVariationalProblem(a_u, L2_u, du)
+solv_u2 = LinearVariationalSolver(prob_u2, solver_parameters=params)
+prob_u3 = LinearVariationalProblem(a_u, L3_u, du)
+solv_u3 = LinearVariationalSolver(prob_u3, solver_parameters=params)
+
+#function to hold current increment of D
+dD = Function(V2)
+solv_D1 = LinearVariationalSolver(prob_u1, solver_parameters=params)
+prob_D1 = LinearVariationalProblem(a_D, L1_D, dD)
+prob_D2 = LinearVariationalProblem(a_D, L2_D, dD)
+solv_D2 = LinearVariationalSolver(prob_u2, solver_parameters=params)
+prob_D3 = LinearVariationalProblem(a_D, L3_D, dD)
+solv_D3 = LinearVariationalSolver(prob_u3, solver_parameters=params)
+
+
+t = 0.0
+step = 0
+output_freq = 10
+while t < T - 0.5*dt:
+    #solving the u equation
+    solv_u1.solve()
+    u1.assign(u + du)
+
+    solv_u2.solve()
+    u2.assign(0.75*u + 0.25*(u1 + du))
+
+    solv_u3.solve()
+    u.assign((1.0/3.0)*u + (2.0/3.0)*(u2 + du))
+
+    #solving the D-equation
+    solv_D1.solve()
+    D1.assign(D + dD)
+
+    solv_D2.solve()
+    D2.assign(0.75*D + 0.25*(D1 + dD))
+
+    solv_D3.solve()
+    D.assign((1.0/3.0)*D + (2.0/3.0)*(D2 + dD))
+
+    
+
+    step += 1
+    t += dt
+    if step % output_freq == 0:
+        Ds.append(D.copy(deepcopy=True))
+        us.append(u.copy(deepcopy=True))
+        print("t=", t)
+
+
+nsp = 16
+fn_plotter = FunctionPlotter(mesh, num_sample_points=nsp)
+
+# We first set up a figure and axes and draw the first frame. ::
+
+fig, axes = plt.subplots()
+axes.set_aspect('equal')
+colors = tripcolor(D_init, num_sample_points=nsp, vmin=1, vmax=2, axes=axes)
+fig.colorbar(colors)
+
+# Now we'll create a function to call in each frame. This function will use the
+# helper object we created before. ::
+
+def animate(q):
+    colors.set_array(fn_plotter(q))
+
+# The last step is to make the animation and save it to a file. ::
+
+interval = 1e3 * output_freq * dt
+animation = FuncAnimation(fig, animate, frames=Ds, interval=interval)
+try:
+    animation.save("DG_advection.mp4", writer="ffmpeg")
+except:
+    print("Failed to write movie! Try installing `ffmpeg`.")
