2378fdee588670f0617a990ab35ab5a64576666c
Kowalczyk <karina.kwlczyk@gmail.com>
2023-05-26 11:17:59 +0100

    projection step added, still a number of problems, test case for sphere (not working yet)

--- time-stepping/first_order_advection_sphere.py
+++ time-stepping/first_order_advection_sphere.py
@@ -5,72 +5,70 @@ from matplotlib.animation import FuncAnimation
 import numpy as np
 
 
-def plot_fd_vector(function, name):
-    fig, axes = plt.subplots()
-    quiver(function, axes=axes)
-    axes.set_aspect("equal")
-    axes.set_title(name)
-    fig.savefig(name)
-
 R0 = 6371220.
+H = Constant(5960.)
 Omega = Constant(7.292e-5)  # rotation rate
-#f = 2*Omega*z/Constant(R0)  # Coriolis parameter
 g = Constant(9.8)  # Gravitational constant
 
-mesh = PeriodicUnitSquareMesh(40, 40)
+distribution_parameters = {"partition": True, "overlap_type": (DistributedMeshOverlapType.VERTEX, 2)}
 
+mesh = IcosahedralSphereMesh(radius=R0,
+                            degree=1,
+                            refinement_level=4,
+                            distribution_parameters = distribution_parameters)
+x = SpatialCoordinate(mesh)
+mesh.init_cell_orientations(x)
 # We set up a function space of discontinous bilinear elements for :math:`q`, and
 # a vector-valued continuous function space for our velocity field. ::
 
-V = FunctionSpace(mesh, "DG", 1)
-W = FunctionSpace(mesh, "BDM", 2)
+
 #velocity space
 element = FiniteElement("BDM", triangle, degree=2)
 V1_broken = FunctionSpace(mesh, BrokenElement(element))
 V1 = FunctionSpace(mesh, element)
 #space for height:
 V2 = FunctionSpace(mesh, "DG", 1)
-W_broken = MixedFunctionSpace((V1_broken,V2))
-W = MixedFunctionSpace((V1,V2))
+W = MixedFunctionSpace((V1_broken,V2))
 
-# We set up the initial velocity field using a simple analytic expression. ::
+# SET UP EXAMPLE
 
-x, y = SpatialCoordinate(mesh)
 
-velocity = as_vector((1., 0))
-ubar = Function(V1).interpolate(velocity)
+u_0 = 20.0  # maximum amplitude of the zonal wind [m/s]
+u_max = Constant(u_0)
+#solid body rotation (?)
+u_expr = as_vector([-u_max*x[1]/R0, u_max*x[0]/R0, 0.0])
+file = File('ubar.pvd')
+ubar = Function(V1).interpolate(u_expr)
+file.write(ubar)
 
-# Now, we set up the cosine-bell--cone--slotted-cylinder initial coniditon. The
-# first four lines declare various parameters relating to the positions of these
-# objects, while the analytic expressions appear in the last three lines. ::
+D_expr = - ((R0 * Omega * u_max + u_max*u_max/2.0)*(x[2]*x[2]/(R0*R0)))/g
+D = Function(V2).interpolate(D_expr)
 
-bell_r0 = 0.15; bell_x0 = 0.25; bell_y0 = 0.5
-cone_r0 = 0.15; cone_x0 = 0.5; cone_y0 = 0.25
-cyl_r0 = 0.15; cyl_x0 = 0.5; cyl_y0 = 0.75
-slot_left = 0.475; slot_right = 0.525; slot_top = 0.85
 
-bell = 0.25*(1+cos(math.pi*min_value(sqrt(pow(x-bell_x0, 2) + pow(y-bell_y0, 2))/bell_r0, 1.0)))
-cone = 1.0 - min_value(sqrt(pow(x-cone_x0, 2) + pow(y-cone_y0, 2))/cyl_r0, 1.0)
-slot_cyl = conditional(sqrt(pow(x-cyl_x0, 2) + pow(y-cyl_y0, 2)) < cyl_r0,
-             conditional(And(And(x > slot_left, x < slot_right), y < slot_top),
-               0.0, 1.0), 0.0)
+# define velocity field to be advected:
+x_c = as_vector([0.5, 0., sqrt(3)/2])
+F_0 = 3.
+l_0 = 1/4
 
-Unp1 = Function(W)
-unp1, Dnp1 = split(Unp1)
+def dist_sphere(x, x_c):
+    return(R0*acos(dot(x,x_c)/R0**2))
 
-Dnp1.interpolate(1.0 + bell)
-unp1.interpolate(as_vector([1+bell, 1+bell]))
-Dn = Function(V2)
-un = Function(V1)
-ubar.assign(unp1)
-Dbar = Function(V2).assign(Dnp1)
+e_theta = as_vector([x[0]*x[2], x[1]*x[2], -x[0]**2 - x[1]**2])
+e_theta = e_theta/(norm(e_theta)+0.0001)
+print("etheta norm:", norm(e_theta))
 
-#to be the solutions, initialised with un, Dn
-D = Function(V2).assign(Dnp1)
-u = Function(V1_broken).assign(unp1)
+F_theta = F_0*exp(-dist_sphere(x,x_c)/l_0**2)
+velocity = F_theta*e_theta
+
+
+u = Function(V1_broken).interpolate(velocity)
+
+print("u is set")
+
 D_init = Function(V2).assign(D)
 u_init = Function(V1_broken).assign(u)
-plot_fd_vector(u_init, "uinit")
+file = File('init.pvd')
+file.write(D_init, u_init)
    
 # Next we'll create a list to store the function values at every timestep so that
 # we can make a movie of them later. ::
@@ -85,7 +83,7 @@ us =[]
 # condition, :math:`q_\mathrm{in}`.  In general, this would be a ``Function``, but
 # here we just use a ``Constant`` value. ::
 
-T = 1.
+T = 2*pi
 dt = T/600.0
 dtc = Constant(dt)
 D_in = Constant(1.0)
@@ -101,8 +99,6 @@ a_D = phi*dD_trial*dx
 du_trial = TrialFunction(V1_broken)
 w = TestFunction(V1_broken)
 a_u = inner(w, du_trial)*dx
-
-
 # The right-hand-side is more interesting.  We define ``n`` to be the built-in
 # ``FacetNormal`` object; a unit normal vector that can be used in integrals over
 # exterior and interior facets.  We next define ``un`` to be an object which is
@@ -110,7 +106,7 @@ a_u = inner(w, du_trial)*dx
 # negative.  This will be useful in the upwind terms. ::
 
 n = FacetNormal(mesh)
-unn = 0.5*(dot(ubar, n) + abs(dot(ubar, n)))
+un = 0.5*(dot(ubar, n) + abs(dot(ubar, n)))
 
 # We now define our right-hand-side form ``L1`` as :math:`\Delta t` times the
 # sum of four integrals.
@@ -118,7 +114,7 @@ unn = 0.5*(dot(ubar, n) + abs(dot(ubar, n)))
 L1test = dtc*(D*div(phi*ubar)*dx
           #- conditional(dot(u, n) < 0, phi*dot(u, n)*q_in, 0.0)*ds
           #- conditional(dot(u, n) > 0, phi*dot(u, n)*q, 0.0)*ds
-          - (phi('+') - phi('-'))*(unn('+')*D('+') - unn('-')*D('-'))*dS)
+          - (phi('+') - phi('-'))*(un('+')*D('+') - un('-')*D('-'))*dS)
 def eq_D(ubar):
     uup = 0.5 * (dot(ubar, n) + abs(dot(ubar, n)))
     return (inner(grad(phi), ubar)*D*dx
@@ -129,15 +125,21 @@ def perp_u_upwind(u):
     Upwind = 0.5 * (sign(dot(u, n)) + 1)
     return Upwind('+')*perp(u('+')) + Upwind('-')*perp(u('-'))
 
+un = 0.5*(dot(ubar, n) + abs(dot(ubar, n)))
 def adv_u(ubar):
     un = 0.5*(dot(ubar, n) + abs(dot(ubar, n)))
 
     return(-inner(div(outer(w, ubar)), u)*dx
            +dot(jump(w), (un('+')*u('+') - un('-')*u('-')))*dS
     )
-L1_D = dtc*(eq_D(ubar))
 
-L1_u = dtc*(-adv_u(ubar))
+eq_u = adv_u(ubar)
+#adjust to sphere
+
+eq_u += un('+')*inner(w('-'), n('+')+n('-'))*inner(u('+'), n('+'))*dS
+eq_u += un('-')*inner(w('+'), n('+')+n('-'))*inner(u('-'), n('-'))*dS
+L1_u = dtc*(-eq_u)
+L1_D = dtc*(eq_D(ubar))
 # In our Runge-Kutta scheme, the first step uses :math:`q^n` to obtain
 # :math:`q^{(1)}`.  We therefore declare similar forms that use :math:`q^{(1)}`
 # to obtain :math:`q^{(2)}`, and :math:`q^{(2)}` to obtain :math:`q^{n+1}`. We
@@ -171,30 +173,6 @@ solv_2_u = LinearVariationalSolver(prob_2_u, solver_parameters=params)
 prob_3_u = LinearVariationalProblem(a_u, L3_u, du)
 solv_3_u = LinearVariationalSolver(prob_3_u, solver_parameters=params)
 
-
-def proj_u():
-    return (-div(v)*g*Dnp1*dx
-            #+ inner(v, f*perp(unp1))*dx # doesn't make sense in 2d?
-    )
-
-def proj_D(Dbar):
-    uup = 0.5 * (dot(unp1, n) + abs(dot(unp1, n)))
-    return (inner(grad(rho), unp1)*Dbar*dx
-            - jump(rho)*(uup('+')*Dbar('+')
-                            - uup('-')*Dbar('-'))*dS)
-
-#make signs consistent
-v, rho = TestFunctions(W)
-a_proj_u = inner(v, unp1 - u)*dx + dtc* proj_u()
-
-a_proj_D = rho*(Dnp1 -D)*dx - dtc*proj_D(Dbar)
-
-a_proj = a_proj_u + a_proj_D
-
-prob = NonlinearVariationalProblem(a_proj, Unp1)
-solver_proj = NonlinearVariationalSolver(prob, solver_parameters=params)
-
-
 # We now run the time loop.  This consists of three Runge-Kutta stages, and every
 # 20 steps we write out the solution to file and print the current time to the
 # terminal. ::
@@ -202,18 +180,9 @@ solver_proj = NonlinearVariationalSolver(prob, solver_parameters=params)
 t = 0.0
 step = 0
 output_freq = 20
-out_file = File("solution.pvd")
+out_file = File("Results/advection_sphere.pvd")
 out_file.write(D, u)
-
-
-
 while t < T - 0.5*dt:
-
-    Dn.assign(Dnp1)
-    un.assign(unp1)
-    Dbar.assign(Dn)
-    ubar.assign(un)
-
     solv_1_D.solve()
     D1.assign(D + dD)
 
@@ -232,11 +201,6 @@ while t < T - 0.5*dt:
     solv_3_u.solve()
     u.assign((1.0/3.0)*u + (2.0/3.0)*(u2 + du))
 
-    # PROJECTION STEP
-    Dnp1.assign(D)
-    unp1.project(u) #u from discontinuous space
-
-    solver_proj.solve()
 
     step += 1
     t += dt
@@ -245,5 +209,10 @@ while t < T - 0.5*dt:
         out_file.write(D,u)
         print("t=", t)
 
+# To check our solution, we display the normalised :math:`L^2` error, by comparing
+# to the initial condition. ::
 
+L2_err = sqrt(assemble((D - D_init)*(D - D_init)*dx))
+L2_init = sqrt(assemble(D_init*D_init*dx))
+print(L2_err/L2_init)
 
