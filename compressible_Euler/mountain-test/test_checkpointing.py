969df0cedb4003046740eaaf07ec573a82e1dcca
Kowalczyk <karina.kwlczyk@gmail.com>
2024-02-19 17:38:31 +0000

    test h5py

--- compressible_Euler/mountain-test/test_checkpointing.py
+++ compressible_Euler/mountain-test/test_checkpointing.py
@@ -1,5 +1,12 @@
 from firedrake import *
+import h5py
+import numpy as np
 
+
+data_matrix = np.random.uniform(-1, 1, size=(10, 3))
+with h5py.File("test_file.hdf5", "w") as data_file:
+    data_file.create_dataset("dataset_name", data=data_matrix)
+'''
 mesh = UnitSquareMesh(20,20)
 V = FunctionSpace(mesh, "CG", 1)
 x, y = SpatialCoordinate(mesh)
@@ -10,3 +17,4 @@ with CheckpointFile("testfile.h5", 'w') as file:
     file.save_mesh(mesh)
     file.save_function(u)
     file.save_function(w)
+'''
