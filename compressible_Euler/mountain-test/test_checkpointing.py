9f68eab791a92579f37eb5079e192e6f942fa557
Kowalczyk <karina.kwlczyk@gmail.com>
2024-02-20 14:19:07 +0000

    test h5

--- compressible_Euler/mountain-test/test_checkpointing.py
+++ compressible_Euler/mountain-test/test_checkpointing.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 data_matrix = np.random.uniform(-1, 1, size=(10, 3))
-with h5py.File("test_file.hdf5", "w") as data_file:
+with h5py.File("test_file.h5", "w") as data_file:
     data_file.create_dataset("dataset_name", data=data_matrix)
 '''
 mesh = UnitSquareMesh(20,20)
