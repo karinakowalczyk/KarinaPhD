9208fa18c48081286221e307770ae5394778ad71
Kowalczyk <karina.kwlczyk@gmail.com>
2024-07-02 11:33:33 +0100

    decreased dt

--- compressible_Euler/convergence tests/convergence_vortex.py
+++ compressible_Euler/convergence tests/convergence_vortex.py
@@ -59,11 +59,12 @@ def exact_solution (x, y, t):
     #    yc -=L
 
     R = 0.4*L #radius of vortex
-    distx = conditional(abs(x-xc)>L/2, abs(x-xc)-L/2, abs(x-xc))
-    disty = conditional(abs(y-yc)>L/2, abs(y-yc)-L/2, abs(y-yc))
+    distx = conditional(abs(x-xc)>L/2, L-abs(x-xc), abs(x-xc))
+    disty = conditional(abs(y-yc)>L/2, L-abs(y-yc), abs(y-yc))
     diffx = x-xc
     diffy = y-yc
-    r = conditional(abs(x-xc) <= L/2, sqrt(diffx*diffx + diffy*diffy)/R, sqrt((L-abs(x-xc))**2 + (L-abs(y-yc))**2)/R)
+    r = sqrt(distx*distx + disty*disty)/R
+    #r = conditional(abs(x-xc) <= L/2, sqrt(diffx*diffx + diffy*diffy)/R, sqrt((L-abs(x-xc))**2 + (L-abs(y-yc))**2)/R)
     print(type(r))
     #phi = atan2(x-xc, y - yc)
 
@@ -72,13 +73,56 @@ def exact_solution (x, y, t):
 
     uth = (1024 * (1.0 - r)**6 * (r)**6)
     #phi = atan2(x-xc, y-yc)
-    diffx = conditional(abs(x-xc) <= L/2, x - xc, L-abs(x-xc))
-    diffy = conditional(abs(y-yc) <= L/2, y - yc, L-abs(y-yc))
+    #diffx = conditional(abs(x-xc) <= L/2, x - xc, L-abs(x-xc))
+    #diffy = conditional(abs(y-yc) <= L/2, y - yc, L-abs(y-yc))
+    signx = conditional(x>xc,-1, +1 )
+    signy = conditional(y>yc,-1, +1 )
+    diffx = conditional(abs(x-xc) <= L/2, x - xc, signx*(L-abs(x-xc)))
+    diffy = conditional(abs(y-yc) <= L/2, y - yc, signy*(L-abs(y-yc)))
     ux = conditional(r>=1, uc, uc - uth * diffy/(r*R))
     
     uy = conditional(r>=1, vc, vc + uth * diffx/(r*R))
     u0 = as_vector([ux,uy])
-    return u0, rhob
+
+    coe = np.zeros((25))
+    coe[0]  =     1.0 / 24.0
+    coe[1]  = -   6.0 / 13.0
+    coe[2]  =    18.0 /  7.0
+    coe[3]  = - 146.0 / 15.0
+    coe[4]  =   219.0 / 8.0
+    coe[5]  = - 966.0 / 17.0
+    coe[6]  =   731.0 /  9.0
+    coe[7]  = -1242.0 / 19.0
+    coe[8]  = -  81.0 / 40.0
+    coe[9]  =   64.
+    coe[10] = - 477.0 / 11.0
+    coe[11] = -1032.0 / 23.0
+    coe[12] =   737.0 / 8.0
+    coe[13] = - 204.0 /  5.0
+    coe[14] = - 510.0 / 13.0
+    coe[15] =  1564.0 / 27.0
+    coe[16] = - 153.0 /  8.0
+    coe[17] = - 450.0 / 29.0
+    coe[18] =   269.0 / 15.0
+    coe[19] = - 174.0 / 31.0
+    coe[20] = -  57.0 / 32.0
+    coe[21] =    74.0 / 33.0
+    coe[22] = -  15.0 / 17.0
+    coe[23] =     6.0 / 35.0
+    coe[24] =  -  1.0 / 72.0
+
+    p = 0
+    for ip in range(25):
+        p += coe[ip] * (r**(12+ip)-1)
+    mach = 0.341
+    p = 1 + 1024**2 * mach**2 *conditional(r>=1, 0, p)
+    R0 = 287.
+    gamma = 1.4
+    kappa = 2./7.
+    pref = Parameters.p_0
+    T = p/(rhob*R0)
+    thetab = T*(pref/p)**kappa
+    return u0, rhob, thetab
 
 #define delx and delt so that delx/delt is constant, since we expect second order 
 #convergence in both, delt and delx 
@@ -86,12 +130,12 @@ uc = 100.
 vc = 100.
 L = 10000.
 H = 10000.
-delx_list = [500., 250., 125., 125/2]
-delt_list = [0.0025]#[0.125/16 for i in range(len(delx_list))]
+delx_list = [125./2]
+delt_list = [0.00025]#[0.125/16 for i in range(len(delx_list))]
 n_timesteps = [100/delt_list[i] for i in range(len(delt_list))]
 nx_list = [int(10000/delx_list[i]) for i in range(len(delx_list))]
 print("nx_list ", nx_list)
-check_freqs= [10 for i in range(len(delx_list))]
+
 
 #delx_list = [500/(2**i) for i in range(4)]
 print("delx_list = ", delx_list)
@@ -119,13 +163,15 @@ for i in range(len(delx_list)):
     x, y = SpatialCoordinate(mesh)
 
     Vc = mesh.coordinates.function_space()
+    print(Vc)
 
     #Vc = VectorFunctionSpace(mesh, "DG", 2)
     x, y = SpatialCoordinate(mesh)
     xc = L/2
     y_m = 300.
     sigma_m = 500.
-    y_s = y_m*exp(-((x - xc)/sigma_m)**2)
+    #y_s = y_m*exp(-((x - xc)/sigma_m)**2)
+    y_s = y_m*cos(2*pi*(x-xc)/L)
     y_expr = y+y_s
     new_coords = Function(Vc).interpolate(as_vector([x,y_expr]))
     mesh.coordinates.assign(new_coords)
@@ -265,15 +311,14 @@ for i in range(len(delx_list)):
     Problem.u0 = u0
     Problem.rhob = rhob
     Problem.Pib = pib
-    Problem.solver_params = sparameters
+    Problem.solver_params = sparameters_star
     Problem.path_out = "../Results/convergence/vortex"+str(nx)
     Problem.thetab = thetab
     Problem.theta_init_pert = 0
     Problem.sponge_fct = False
-    Problem.checkpointing = True
+    Problem.checkpointing = False
     Problem.checkpoint_path = "checkpointVortex"+str(nx)+".h5"
-    Problem.check_freq = check_freqs[i]
-    Problem.out_error =  True
+    
     #Problem.error_file = "../Results/convergence/vortex"
     Problem.exact_sol = exact_solution
 
@@ -287,7 +332,7 @@ for i in range(len(delx_list)):
 
 
     tmax = 10.
-    dumpt = 100*dt
+    dumpt = 40*dt
     print ("courant = ", uc*dt/delx)
 
     Problem.solve(dt=dt, tmax=tmax, dumpt=dumpt, hydrostatic_balance_background=False)
