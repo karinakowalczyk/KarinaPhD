05caf53b86d37c716634aad8d7a19a54b0f267c5
Kowalczyk <karina.kwlczyk@gmail.com>
2024-07-09 13:59:26 +0100

    smaller dt

--- compressible_Euler/convergence tests/convergence_vortex.py
+++ compressible_Euler/convergence tests/convergence_vortex.py
@@ -332,7 +332,7 @@ for i in range(len(delx_list)):
 
 
     tmax = 10.
-    dumpt = 1000*dt
+    dumpt = 2000*dt
     print ("courant = ", uc*dt/delx)
 
     Problem.solve(dt=dt, tmax=tmax, dumpt=dumpt, hydrostatic_balance_background=False)
