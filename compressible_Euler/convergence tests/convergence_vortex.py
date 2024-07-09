07b7a8a774d3c424e6b5cb2741a1f9ebadc4fe0b
Kowalczyk <karina.kwlczyk@gmail.com>
2024-07-09 13:57:05 +0100

    smaller dt

--- compressible_Euler/convergence tests/convergence_vortex.py
+++ compressible_Euler/convergence tests/convergence_vortex.py
@@ -131,7 +131,7 @@ vc = 100.
 L = 10000.
 H = 10000.
 delx_list = [80.]
-delt_list = [0.00005]#[0.125/16 for i in range(len(delx_list))]
+delt_list = [0.001/2]#[0.125/16 for i in range(len(delx_list))]
 n_timesteps = [100/delt_list[i] for i in range(len(delt_list))]
 nx_list = [int(10000/delx_list[i]) for i in range(len(delx_list))]
 print("nx_list ", nx_list)
@@ -170,8 +170,8 @@ for i in range(len(delx_list)):
     xc = L/2
     y_m = 300.
     sigma_m = 500.
-    #y_s = y_m*exp(-((x - xc)/sigma_m)**2)
-    y_s = y_m*cos(2*pi*(x-xc)/L)
+    y_s = y_m*exp(-((x - xc)/sigma_m)**2)
+    #y_s = y_m*cos(2*pi*(x-xc)/L)
     y_expr = y+y_s
     new_coords = Function(Vc).interpolate(as_vector([x,y_expr]))
     mesh.coordinates.assign(new_coords)
@@ -331,8 +331,8 @@ for i in range(len(delx_list)):
     print("dt = ", dt)
 
 
-    tmax = 2.
-    dumpt = 200*dt
+    tmax = 10.
+    dumpt = 1000*dt
     print ("courant = ", uc*dt/delx)
 
     Problem.solve(dt=dt, tmax=tmax, dumpt=dumpt, hydrostatic_balance_background=False)
